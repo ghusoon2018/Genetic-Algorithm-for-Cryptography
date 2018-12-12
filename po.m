4b2de0bba803a20ede103eea6bede2eb8efa8b9f
Gusoon <37520230+ghusoon2018@users.noreply.github.com>
2018-12-12 18:38:23 +0800

    Add files via upload

---
+++ po.m
@@ -0,0 +1,14 @@
+function[pop1]=po();
+for i=1:10
+    pop1(i,:)=randperm(26);
+end
+save pop1 pop1
+disp('pop1');
+disp('pop1')
+function[pop1]=po();
+for i=1:10
+    pop1(i,:)=randperm(26);
+end
+save pop1 pop1
+disp('pop1');
+disp('pop1')
